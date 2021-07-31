c9325f1ba1eb91ea58070159fa6d7a5bbd68b413
ChaitanyaD <crdhuware07@gmail.com>
2021-07-31 14:23:05 +0530

    fix

--- javascript.js
+++ javascript.js
@@ -1,8 +1,10 @@
 var addItem=0;
 function addTocart(item){
     addItem=addItem+1;
+   
     var selectedItem =document.createElement('div');
     selectedItem.classList.add('cartImg');
+    selectedItem.setAttribute('class','card');
     selectedItem.setAttribute('id',addItem);
      var img=document.createElement('img');
      
@@ -10,17 +12,20 @@ function addTocart(item){
 
 var title =document.createElement('div');
 title.innerText=item.children[1].children[0].innerText;
-
+title.setAttribute('class','card-title');
 var price =document.createElement('div');
 price.innerHTML=item.children[1].children[1].innerText;
 
 var delbtn=document.createElement('button');
 delbtn.innerText='Delete';
 delbtn.setAttribute('onClick', 'del('+addItem+')');
-delbtn.setAttribute('class','btn-danger');
+delbtn.setAttribute('class','btn-danger ');
 
 
     var cartItems=document.getElementById('title');
+   
+
+    cartItems.setAttribute('class','card-group ');
     selectedItem.append(img);
     selectedItem.append(title);
     selectedItem.append(price);
